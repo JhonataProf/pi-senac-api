99bbead479651f9108ade3548ce06da6c92af79a
Carlos Bezerra <carlosmiguelcm820@gmail.com>
2024-02-16 19:29:49 -0400

    Update do PR 16/02

--- src/infra/db/mysql/data-source.ts
+++ src/infra/db/mysql/data-source.ts
@@ -9,7 +9,7 @@ const SnakeNamingStrategy =
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
-  port: 3312,
+  port: 3306,
   username: 'root',
   password: 'admin',
   database: 'sistema_podologia',
